d32b1a4c7c5a12ae095ad7ed414f35237b191fa1
tuan <tincanxh@gmail.com>
2018-04-29 22:17:49 +0700

    [Thực hành] Ứng dụng Từ điển đơn giản - Servlet

---
+++ out/artifacts/simple_dictionary_war_exploded/index.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tinca
+  Date: 4/27/2018
+  Time: 4:05 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <form method="get" action="/cc">
+    <div class="dictionary">
+      <h1>Vietnamese Dictionary</h1>
+      <input type="text" name="english" size="30" placeholder="Enter English">
+      <input type="submit" value="Submit">
+    </div>
+  </form>
+  </body>
+</html>
---
+++ src/TranslationServlet.java
@@ -0,0 +1,35 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "TranslationServlet", urlPatterns = "/cc")
+public class TranslationServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("small", "nhỏ bé");
+        dictionary.put("big", "to lớn");
+        String search = request.getParameter("english");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        String result = dictionary.get(search);
+        if (result != null) {
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+        writer.println("</html>");
+    }
+}
